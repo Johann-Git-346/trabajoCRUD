23851e3ceefedfbf4d912508fded1eeda48a11a6
CristoferAcevedo <158364057+CristoferAcevedo@users.noreply.github.com>
2024-08-26 15:45:19 -0500

    modelo version 2

--- ModeloInicio.py
+++ ModeloInicio.py
@@ -6,15 +6,18 @@ class ModeloUsuario:
         if self.connection:
             cursor = self.connection.cursor()
             try:
-                cursor.execute("SELECT * FROM usuarios WHERE email = %s", (email,))
+                cursor.execute("SELECT * FROM usuario WHERE email = %s", (email,))
                 if cursor.fetchone():
                     cursor.close()
                     return False  # El correo ya está registrado
 
                 # Asigna el rol según la selección del usuario
-                rol_id = 2 if rol == 'Vendedor' else 3  # 2 para Vendedor, 3 para Usuario
+                rol_id = 1 if rol == 'Administrador' else (2 if rol == 'Vendedor' else 3) #<-- lo hice para ver si servia lo de la vista administrador
+                #si va a usar el suyo no elimine el mio comentelo como yo estoy haciendo.
+                # Y NO CREE OTRO ARCHIVO CON EL MISMO NOMBRE....
+                #rol_id = 2 if rol == 'Vendedor' else 3  # 2 para Vendedor, 3 para Usuario <-- este es el suyo JULIAN
 
-                cursor.execute("INSERT INTO usuarios (email, password, rol_id) VALUES (%s, %s, %s)", 
+                cursor.execute("INSERT INTO usuario (email, password, rol_id) VALUES (%s, %s, %s)", 
                                (email, contrasena, rol_id))
                 self.connection.commit()
                 cursor.close()
@@ -30,8 +33,8 @@ class ModeloUsuario:
         if self.connection:
             cursor = self.connection.cursor()
             cursor.execute("""
-                SELECT r.nombre FROM usuarios u
-                JOIN roles r ON u.rol_id = r.id
+                SELECT r.nombre FROM usuario u
+                JOIN rol r ON u.rol_id = r.id
                 WHERE u.email = %s AND u.password = %s
             """, (email, contrasena))
             resultado = cursor.fetchone()
