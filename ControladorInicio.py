1441fbef9f90b0e5987b11efec3361d7669e342f
CristoferAcevedo <158364057+CristoferAcevedo@users.noreply.github.com>
2024-09-16 18:15:03 -0500

    controladorMejorado

--- ControladorInicio.py
+++ ControladorInicio.py
@@ -1,16 +1,21 @@
-from ModeloInicio import ModeloUsuario
-from VistaInicio import VistaUsuario
+from baseDatos import Crear_Conexion
+from modelo import ModeloUsuario
+from vista import VistaUsuario
 from vistaProductos import Davista
 from VistaTablas import VistaTablas1
 from VistaVendedor import Davista2
 
 class Controlador:
-    def __init__(self, conexion):
-        self.modelo = ModeloUsuario(conexion)
-        self.vista = VistaUsuario(self)
+    def __init__(self, conexion,modelo):
+        self.conexion=conexion
+        self.modelo = modelo
+        
 
-    def iniciar(self):
-        self.vista.iniciar()
+    def obtener_productos(self):
+        return self.modelo.obtener_productos()
+    
+    def subirImagen(self,nombre,imagen):
+        return self.modelo.imagenABaseDatos(nombre, imagen )
 
     def registrar(self, email, contrasena, rol):
         return self.modelo.registrar_usuario(email, contrasena, rol)
@@ -20,7 +25,7 @@ class Controlador:
         if rol:
             #si quiere que le sirva cambie lo que esta en las comillas por lo que tiene en la base de datos
             if rol == 'ADMINISTRA':#<-- esto lo cambia a minusculas tal cual como lo tiene en la base de datos.
-                self.mostrar_vista_admin()
+                self.mostrar_vista_vendedor()
             elif rol == 'VENDEDOR':
                 self.mostrar_vista_vendedor()
             elif rol == 'CLIENTE':
@@ -28,12 +33,29 @@ class Controlador:
             return rol
         else:
             return None
-
-    def mostrar_vista_admin(self):
-        tablas=VistaTablas1()
-
     def mostrar_vista_vendedor(self):
-        app = Davista2()
+        vistaVendedor.iniciarVendedor()
 
     def mostrar_vista_cliente(self):
-        app = Davista()
+        vistaProductos.iniciarProductos()
+
+    def vistaInformes(self):
+        vistaTablas.iniciarTablas()
+
+    def mostrarLogin(self):
+        vistaUsuario.iniciarUsuario()
+
+
+
+# PRINCIPAL
+conexion = Crear_Conexion.conexionBaseDeDatos()
+if conexion:
+    modelo=ModeloUsuario(conexion)    
+    controlador = Controlador(conexion,modelo)
+    vistaProductos=Davista(controlador)
+    vistaTablas=VistaTablas1(controlador)
+    vistaVendedor=Davista2(controlador)
+    vistaUsuario=VistaUsuario(controlador)
+    vistaUsuario.iniciarUsuario()
+else:
+    print("No se pudo establecer conexión con la base de datos.")
