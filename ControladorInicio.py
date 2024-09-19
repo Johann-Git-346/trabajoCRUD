bae9e7e56851ba3665fc4f0807219f9d0be285ea
CristoferAcevedo <158364057+CristoferAcevedo@users.noreply.github.com>
2024-09-19 18:49:07 -0500

    controllerV3

--- ControladorInicio.py
+++ ControladorInicio.py
@@ -10,14 +10,15 @@ class Controlador:
         self.conexion=conexion
         self.modelo = modelo
         
+
+    def obtener_productos(self):
+        return self.modelo.obtener_productos()
+    
     def agregar_productos(self,nombre,precio,categoria,cantidad):
         return self.modelo.agregar_producto(nombre,precio,categoria,cantidad)
     
     def subirImagen(self,nombre,imagen):
         return self.modelo.imagenABaseDatos(nombre, imagen )
-    
-    def obtener_productos(self):
-        return self.modelo.obtener_productos()
 
     def registrar(self, email, contrasena, rol):
         return self.modelo.registrar_usuario(email, contrasena, rol)
@@ -35,6 +36,22 @@ class Controlador:
             return rol
         else:
             return None
+        
+    def obtener_mas_vendidos(self):
+        return self.modelo.obtenerMasVendidos()
+    
+    def obtener_menos_vendidos(self):
+        return self.modelo.obtenerMenosVendidos()
+    
+    def modificar_producto(self, nombre,nuevo_precio=None, nueva_descripcion=None, nueva_cantidad=None):
+        self.modelo.modificar_producto(nombre, nuevo_precio, nueva_descripcion, nueva_cantidad)
+
+    def eliminar_producto(self, nombre):
+        self.modelo.eliminar_producto(nombre)
+
+    def obtener_producto_por_nombre(self, nombre):
+        return self.modelo.obtener_producto_por_nombre(nombre)
+
     def mostrar_vista_vendedor(self):
         vistaVendedor.iniciarVendedor()
 
@@ -46,8 +63,6 @@ class Controlador:
 
     def mostrarLogin(self):
         vistaUsuario.iniciarUsuario()
-
-
 
 # PRINCIPAL
 conexion = Crear_Conexion.conexionBaseDeDatos()
