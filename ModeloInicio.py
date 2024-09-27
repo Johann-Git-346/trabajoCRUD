65fcb24dad2942f3ced1167e6de04698d1601579
JULIANBONETT <bonettjulian02@gmail.com>
2024-09-27 09:11:10 -0500

    .

--- ModeloInicio.py
+++ ModeloInicio.py
@@ -80,7 +80,7 @@ class ModeloUsuario:
             cursor = self.connection.cursor()
             cursor.execute("""
                 SELECT r.nombre FROM usuario u
-                JOIN rol r ON u.rol_id = r.id
+                JOIN rol r ON u.Id_rol= r.id
                 WHERE u.email = %s AND u.password = %s
 
             """, (email, contrasena))
